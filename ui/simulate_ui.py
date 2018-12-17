f63446dda574407b0d79089180621f23afc4f00b
cyzhao <471038339@qq.com>
2018-12-17 22:42:53 +0800

    update to mac

--- ui/simulate_ui.py
+++ ui/simulate_ui.py
@@ -9,7 +9,7 @@ class simulation_panel(bpy.types.Panel):
     bl_category = "PhysiKa"
     bl_label = "PhysiKa"
     
-
+    #deteminate when the panel will show
     @classmethod
     def poll(cls, context):
         obj_props = context.scene.objects.active.physika
@@ -19,52 +19,14 @@ class simulation_panel(bpy.types.Panel):
         layout = self.layout
 
         scene = context.scene
-
-        # Create a simple row.
-        layout.label(text=" Simple Row:")
-
-        row = layout.row()
-        row.prop(scene, "frame_start")
-        row.prop(scene, "frame_end")
-
-        # Create an row where the buttons are aligned to each other.
-        layout.label(text=" Aligned Row:")
-
-        row = layout.row(align=True)
-        row.prop(scene, "frame_start")
-        row.prop(scene, "frame_end")
-
-        # Create two columns, by using a split layout.
-        split = layout.split()
-
-        # First column
-        col = split.column()
-        col.label(text="Column One:")
-        col.prop(scene, "frame_end")
-        col.prop(scene, "frame_start")
-
-        # Second column, aligned
-        col = split.column(align=True)
-        col.label(text="Column Two:")
-        col.prop(scene, "frame_start")
-        col.prop(scene, "frame_end")
-
-        # Big render button
-        layout.label(text="Big Button:")
+        
+        # Big Bake
+        layout.label(text="Bake:")
         row = layout.row()
         row.scale_y = 3.0
         row.operator("render.render")
 
-        # Different sizes in a row
-        layout.label(text="Different button sizes:")
-        row = layout.row(align=True)
-        row.operator("render.render")
 
-        sub = row.row()
-        sub.scale_x = 2.0
-        sub.operator("render.render")
-
-        row.operator("render.render")
 
 
 def register():
