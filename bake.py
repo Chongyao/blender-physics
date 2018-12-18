ed2ba5836e1c8886058ab3e33f67df091505cac1
cyzhao <471038339@qq.com>
2018-12-18 21:12:59 +0800

    add simple program and call it as subprogress

---
+++ bake.py
@@ -0,0 +1,12 @@
+import bpy,subprocess,shlex
+
+
+
+def bake():
+    """ Wirte by this way temporarily"""
+    physika_exe_path = "/home/zcy/.config/blender/2.79/scripts/addons/blender-physics/lib/simple-translation/build/"
+    physika_exe = "simple-translation"
+    exe_cmd = physika_exe_path + physika_exe  + " " + physika_exe_path + "input.obj" + " " + physika_exe_path + "output"
+    args = shlex.split(exe_cmd)
+    
+    subprocess.run(args)
--- operators/bake_operators.py
+++ operators/bake_operators.py
@@ -1,14 +1,28 @@
 import bpy
 
+from .. import bake
+
 class BakePhysiKaSimulation(bpy.types.Operator):
     bl_idname = "physika_operators.bake_physika_simulation"
     bl_label = "Bake Fluid Simulation"
     bl_description = "Run fluid simulation"
     bl_options = {'REGISTER'}
 
-    def execute(self, context):
-        return {'RUNNING_MODAL'}
 
+    def export_model(self):
+        filepath = '.config/blender/2.79/scripts/addons/blender-physics/lib/simple-translation/build/input.obj'
+        bpy.ops.export_scene.obj(filepath = filepath, use_materials=False, use_triangles=True, use_normals=False, use_uvs=False)
+        
+    def run_simulation(self, obj):
+        self.export_model();
+        bake.bake()
+    
+    def execute(self, context):
+        print(context.scene)
+        obj = context.scene.objects.active
+        if obj.physika.is_active is True:
+            self.run_simulation(obj)
+        return {'RUNNING_MODAL'}
 def register():
     bpy.utils.register_class(BakePhysiKaSimulation)
 
