7a9adde0e412c4103b2df7ac6dc6fff2ba2950f4
Chongyao <zcyqqxq@vip.qq.com>
2018-12-21 19:10:28 +0800

    add Animate Panel

--- bake.py
+++ bake.py
@@ -5,8 +5,8 @@ import bpy,subprocess,shlex,os,pathlib
 def bake():
     """ Wirte by this way temporarily"""
     raw_path = os.getcwd()
-    
     script_path = os.path.dirname(os.path.realpath(__file__))
+    assert os.path.basename(script_path) is 'blender-physics'
     os.chdir(script_path)
     os.chdir('lib/simple-translation/build')
     
