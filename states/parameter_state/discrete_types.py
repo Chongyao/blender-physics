d949b80e424221234bfdadb7988eb9009edce9cc
cyzhao <471038339@qq.com>
2018-12-27 19:42:20 +0800

    add common para

--- states/parameter_state/discrete_types.py
+++ states/parameter_state/discrete_types.py
@@ -3,7 +3,7 @@ discrete_method = (
     ('METHOD_MASS_SPRING', 'mass_spring', 'mass spring method'),
     ('METHOD_MESHLESS', 'meshless', 'meshless method')
 )
-
+#(para_name, data_type, if exposed, (Enum))
 fem_parameter = ()
 mass_spring_parameter = ()
 meshless_parameter = ()
--- states/parameter_state/set_parameter.py
+++ states/parameter_state/set_parameter.py
@@ -18,6 +18,35 @@ class physika_para(bpy.types.PropertyGroup):
             items = discrete_types.discrete_method,
             # default = 'mass_spring'
         )
+        cls.frames = IntProperty(
+            name = "num of frames",
+            subtype = 'UNSIGNED',
+            default = 50,
+            step = 10
+        )
+        cls.delt_t = FloatProperty(
+            name = "delt_t",
+            description = 'delt time between each iteraton step instead of delt time between each frame',
+            min = 0.01,
+            max = 0.1,
+            step = 1
+        )
+        
+        cls.frame_rate = IntProperty(
+            name = 'frame rate',
+            description = 'num of frames per second',
+            default = 10,
+            subtype = "UNSIGNED",
+            max = 100,
+            update = lambda self, context: self._update_frame_rate(context)
+        )
+
+        def _update_frame_rate(self, context):
+            if self.frame_rate >  1/self.delt_t:
+                self.frame_rate = int(1/self.delt_t)
+        
+            
+        
     @classmethod
     def unregister(cls):
         del bpy.types.Scene.physika_discrete
@@ -25,7 +54,13 @@ class physika_para(bpy.types.PropertyGroup):
 
 # class union_prameter():
 #     class
-    
+def add_specific_discrete_properties():
+    for enum in discrete_types.discrete_method:
+        exec('class physika_para_' + enum[1] + '(bpy.types.PropertyGroup):pass')
+        for paras in eval('discrete_type.' + enum[1] + '_parameter'):
+            
+            pass   
+        setattr(physika_para, enum[1], PointerProperty(type=eval('physika_para_' + enum[1])))
 
 def register():
     bpy.utils.register_class(physika_para)
